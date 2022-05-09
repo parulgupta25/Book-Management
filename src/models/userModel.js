76918316e4fc37341db303d7edf36b5ef11f9aec
parulgupta25 <parulg1999@gmail.com>
2022-05-09 17:34:49 +0530

    Project3

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    title: { type: string, required: true, enum: [Mr, Mrs, Miss]},
-    name: { type: string, required: true},
-    phone: { type: string, required: true, unique: true},
-    email: { type: string, required: true, unique: true}, 
-    password: { type: string, required: true, minLength: 8, maxlength: 15},
+    title: { type: String, required: true, enum: ["Mr", "Mrs", "Miss"]},
+    name: { type: String, required: true},
+    phone: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true}, 
+    password: { type: String, required: true, minLength: 8, maxLength: 15},
     address: {
-        street: { type: string},
-        city: { type: string},
-        pincode: { type: string}
+        street: { type: String},
+        city: { type: String},
+        pincode: { type: String}
     }
 }, { timestamps: true });
 

adc42304041b48b051b6e777e3406ca8ee92392b
Bhagawaan <bhagabanghadai043@gmail.com>
2022-05-16 14:57:34 +0530

    Book-Management

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -42,7 +42,7 @@ try{
         return res.status(400).send({ status: false, message: "Invalid userId." })
     }
     
-    let checkBook=await bookModel.findById(book_id)
+    let checkBook=await bookModel.findById(book_id).lean()
 
     if(!checkBook){
         return res.status(400).send({ status: false, message: "BookId Not Found" })
@@ -59,8 +59,9 @@ try{
 
     const reviewDetails = await reviewModel.findOne({ _id: saveReview._id }).select({__v: 0,createdAt: 0,updatedAt: 0,isDeleted: 0})
     
+    checkBook.reviewsData=reviewDetails
 
-    res.status(201).send({ status: true, message: "review created successfully", data: reviewDetails })
+    res.status(201).send({ status: true, message: "review created successfully", data: checkBook })
 }
 catch(err){
     res.status(500).send({ status: false, Error: err.message })
@@ -103,11 +104,11 @@ try{
     let checkReview=await reviewModel.findById(review_Id)
 
     if(!checkBook){
-        return res.status(400).send({ status: false, message: "BookId Not Found" })
+        return res.status(404).send({ status: false, message: "BookId Not Found" })
     }
 
     if(!checkReview){
-        return res.status(400).send({ status: false, message: "reviewId Not Found" })
+        return res.status(404).send({ status: false, message: "reviewId Not Found" })
     }
 
     if (checkBook.isDeleted == true||checkReview.isDeleted==true){
